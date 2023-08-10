05596e923903bcf8c5bc5687af26cac5d0ee95bc
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-10 09:47:15 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,9 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+		char quote1[] = "and that piece of art is useful\"";
+		char quote2[] = "Dora Korpar, 2015-10-19\n";
+
+		fprintf(stderr, "%s - %s", quote1, quote2);
+		return (1);
 }
