209a1c8ad72e0e7556efeaef327b9da82cadad49
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-10 08:12:57 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Text
+ */
+int main(void)
+{
+		int someInt;
+		float someFloat;
+		long int someLongInt;
+		long long int someLongLongInt;
+		char someChar;
+
+		printf("Size of a char: %ld byte(s)\n", sizeof(someChar));
+		printf("Size of a int: %ld byte(s)\n", sizeof(someInt));
+		printf("Size of a long int: %ld byte(s)\n", sizeof(someLongInt));
+		printf("Size of a long longi int: %ld byte(s)\n", sizeof(someLongLongInt));
+		printf("Size of a float: %ld byte(s)\n", sizeof(someFloat));
+		return (0);
+}
