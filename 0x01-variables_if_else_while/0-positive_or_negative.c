3b1c6b2f7ddd7334e25dbcf2896503b2fac44d48
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-11 03:17:57 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,9 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	char *a = "is positive";
-	char *b = "is zero";
-	char *c = "is negative";
+	char *a;
+	a= "is positive";
+	char *b;
+       	b = "is zero";
+	char *c; 
+	c = "is negative";
 
 	printf("%d %s\n", n, n > 0 ? a : (n == 0 ? b : c));
 	return (0);
