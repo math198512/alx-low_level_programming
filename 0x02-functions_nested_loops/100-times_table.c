59ed1e9a33d4efdb0bb01bdedfdb05bc68f0a533
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-15 15:26:56 -0700

    100-times_table.c and main.h

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 		for (j = 0; j <= n; j++)
 		{
 			_putchar(48);
-			_putchar(n != 0 ? ',' : NULL);
+			_putchar(n != 0 ? ',' : 0);
 			for (i = 1; i <= n; i++)
 			{
 				p = j * i;
