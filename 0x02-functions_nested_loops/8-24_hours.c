342016fdbf6754f7ca68253bccb30f3f27b9624a
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-15 06:57:28 -0700

    8-24_hours.c and main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer.
+ * * Return: 0, success.
+ */
+void jack_bauer(void)
+{
+	int a, b;
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (a = 0; a <= 23 ; a++)
+	{
+		i = a / 10;
+		j = a % 10;
+		for (b = 0; b <= 59 ; b++)
+                {
+                        k = b / 10;
+                        l = b % 10;
+                        if (i <= k || (i == k && j < l))
+                        {
+                                _putchar(i + '0');
+                                _putchar(j + '0');
+                                _putchar(':');
+                                _putchar(k + '0');
+                                _putchar(l + '0');
+                                if (i == 9 && j == 8)
+                                {
+                                        break;
+                                }
+                                _putchar('\n');
+                        }
+                }
+        }
+}
