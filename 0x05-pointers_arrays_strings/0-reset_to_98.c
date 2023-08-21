83fe3ccfbd05c29b618b81f4f84ba1b772e42356
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-21 00:22:46 -0700

    task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * and updates the value it points to to 98.
- * @*n: input parameter.
+ * @n: input parameter.
 *Return: nothing.
 */
 void reset_to_98(int *n)
