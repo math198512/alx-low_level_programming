49be3c09beb0a3c21b5ec5f5f80be9916e683194
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-21 17:28:37 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string, followed by a new line.
+ * @str: string pointer.
+ * Return: nothing.
+*/
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+	_putchar('\n');
+}
