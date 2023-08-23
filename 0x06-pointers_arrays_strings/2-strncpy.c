d4eb3c86d1dcf2117efa41d5360fedff601d5526
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-22 23:47:27 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string to another
+ * @src: char string to concatenate to
+ * @dest: char string
+ * @n: number of elements to copy.
+ * Return: pointer to resulting string `dest`
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
