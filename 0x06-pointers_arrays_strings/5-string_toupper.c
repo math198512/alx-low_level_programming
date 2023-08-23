98c4517c001cf9dd008ab0f751e871940c71b3a8
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-23 11:11:00 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase.
+ * @str: input string.
+ * Return: 0.
+ */
+
+char *string_toupper(char *str)
+{
+	int i, j;
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; j < i; j++)
+	{
+		if (str[j] > 96 && str[j] < 123)
+		{
+			str[j] -= 32;
+		}
+	}
+	return (str);
+}
