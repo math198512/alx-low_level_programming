91c7767eb54ad348cd65546843e7f446e8aa65d1
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-26 04:47:19 -0700

    task 12

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints an integer.
+ * @n: input integer to be printed.
+ * Return: nothing.
+ */
+
+void print_number(int n)
+{
+	unsigned int number;
+
+	number = n;
+	if (n < 0)
+	{
+		_putchar('-');
+		number = -n;
+	}
+	if ((number / 10) > 0)
+	{
+		print_number(number / 10);
+	}
+	_putchar((number % 10) + '0');
+}
