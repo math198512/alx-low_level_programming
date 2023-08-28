bd248a118f73c3cc7113f904c5f3e664a985aa3d
Tarik HORAICHI <thoraichi@gmail.com>
2023-08-28 10:29:09 -0700

    task 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print chessboard
+ * @a: pointer to chessboard content.
+ * Return: void.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	for (j = 0; j < 8; j++)
+	{
+		for (i = 0; i < 8; i++)
+		{
+			_putchar(a[j][i]);
+		}
+		_putchar('\n');
+	}
+}
