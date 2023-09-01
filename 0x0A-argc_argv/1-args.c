20ada0606a2307c9cf67bf4ada30622ee71b41d5
Tarik HORAICHI <thoraichi@gmail.com>
2023-09-01 15:42:56 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the number of arguments passed into it
+*@argc: number of arguments
+*Return: 0
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
