e8ac8d45470f97eb4cc93f6c4a69d7e6125df8d1
Tarik HORAICHI <thoraichi@gmail.com>
2023-09-02 05:55:13 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -20,18 +20,15 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
-		if (argv[i][j] < '0' || argv[i][j] > '9')
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			res += atoi(argv[i]);
-		}
-
+		res += atoi(argv[i]);
 	}
-
 	printf("%d\n", res);
 	return (2);
 }
