e838389a40dd8ad1d07d91807ac7cd1b486182a6
Tarik HORAICHI <thoraichi@gmail.com>
2023-09-08 05:20:17 -0700

    task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -38,7 +38,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		{
 			return (NULL);
 		}
-		for (i = 0; i < new_size || i < old_size; i++)
+		for (i = 0; i < min(old_size, new_size); i++)
 		{
 			*((char *)new_ptr + i) = *((char *) ptr + i);
 		}
