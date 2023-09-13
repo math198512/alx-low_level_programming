a98d2ed1635ba872507a2072ffeac2c43a3a95cf
Tarik HORAICHI <thoraichi@gmail.com>
2023-09-13 05:11:08 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,11 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: function which print name
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
