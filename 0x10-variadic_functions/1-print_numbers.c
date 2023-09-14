820ff64187ad0da82c37a4db6205c7cd34a13ee0
Tarik HORAICHI <thoraichi@gmail.com>
2023-09-14 09:22:00 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -23,7 +23,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		int x = va_arg(args, int);
 
-		printf("%i", x);
+		printf("%d", x);
 		if (i != (n - 1))
 		{
 			printf("%s", separator);
