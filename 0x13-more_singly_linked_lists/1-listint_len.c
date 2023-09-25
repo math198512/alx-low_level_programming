c31172d95fc2afbd18a61a13982df399a7f39526
math198512 <thoraichi@gmail.com>
2023-09-25 14:26:47 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - find the number of elements in a linked `listint_t` list
+ * @h: linked list head
+ * Return: number of elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *temp;
+	size_t count;
+
+	for (temp = h, count = 0; temp != NULL; count++, temp = temp->next)
+		;
+
+	return (count);
+}
