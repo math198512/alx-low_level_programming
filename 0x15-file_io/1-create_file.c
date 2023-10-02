8e6418cb84b9b1a2e6a36abc9215a6ebb7eaa81c
math198512 <thoraichi@gmail.com>
2023-10-02 14:34:22 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,12 +9,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file_handle;
+	int o, w, len = 0;
 
-	file_handle = fopen("filename", "w");
-	fputs(text_content, file_handle);
+	if (filename == NULL)
+		return (-1);
 
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
-	fclose(file_handle);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
 
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
 }
