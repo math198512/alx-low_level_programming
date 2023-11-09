2e6d48d35ab9a9768b3d0365880d268a607c53f4
math198512 <thoraichi@gmail.com>
2023-11-09 07:26:23 -0800

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,21 +7,18 @@
  * Return: number of nodes
  */
 
+
 size_t dlistint_len(const dlistint_t *h)
 {
-
 	int counter = 0;
 
 	if (h == NULL)
 		return (counter);
 
-	while (h->prev != NULL)
-		h = h->prev;
-
 	while (h != NULL)
 	{
-		h = h->next;
 		counter++;
+		h = h->next;
 	}
 
 	return (counter);
