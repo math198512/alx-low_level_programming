3f35fed7f0ac867a87c609dedb19d85d30ea0a62
math198512 <thoraichi@gmail.com>
2023-11-10 15:12:25 -0800

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -9,9 +9,11 @@
 int sum_dlistint(dlistint_t *head)
 {
 	int sum;
-	dlistint_t *temp;
-	temp = head;
-	for (sum = 0; temp; temp = temp->next)
-		sum += temp->n;
+	dlistint_t *tmp;
+
+	tmp = head;
+	for (sum = 0; tmp; tmp = tmp->next)
+		sum += tmp->n;
+
 	return (sum);
 }
